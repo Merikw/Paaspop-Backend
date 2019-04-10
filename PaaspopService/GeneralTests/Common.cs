e936fe4bbcdd25cd93a1c5dcb0bf88658e49e28b
Merik Westerveld <merik.westerveld@student.fontys.nl>
2019-04-10 08:10:27 +0200

    Change tests

--- PaaspopService/GeneralTests/Common.cs
+++ PaaspopService/GeneralTests/Common.cs
@@ -15,15 +15,13 @@ namespace GeneralTests
             var resultTrue = BetweenHandler.IsInBetween(4, 1, 10);
             var resultFalse = BetweenHandler.IsInBetween(0, 1, 10);
             var resultTrueNegative = BetweenHandler.IsInBetween(-4, -10, 1);
-            var resultFalseNegative = BetweenHandler.IsInBetween(-11, -10, 1);
-            var resultFalseMinBoundary = BetweenHandler.IsInBetween(-10, -10, 1);
-            var resultFalseMaxBoundary = BetweenHandler.IsInBetween(1, -10, 1);
+            var resultFalseMinBoundary = BetweenHandler.IsInBetween(-11, -10, 1);
+            var resultFalseMaxBoundary = BetweenHandler.IsInBetween(2, -10, 1);
 
             // Assert
             resultTrue.Should().Be(true);
             resultFalse.Should().Be(false);
             resultTrueNegative.Should().Be(true);
-            resultFalseNegative.Should().Be(false);
             resultFalseMinBoundary.Should().Be(false);
             resultFalseMaxBoundary.Should().Be(false);
         }
